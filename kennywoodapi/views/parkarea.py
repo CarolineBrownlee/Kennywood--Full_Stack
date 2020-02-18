705678a3ad45f8a3fcc44078ca1d76091ef4efe3
Caroline Brownlee <brownleecaroline@gmail.com>
2020-02-18 04:41:21 -0600

    added create, delete, and edit methods to parkarea.py

--- kennywoodapi/views/parkarea.py
+++ kennywoodapi/views/parkarea.py
@@ -25,6 +25,22 @@ class ParkAreaSerializer(serializers.HyperlinkedModelSerializer):
 class ParkAreas(ViewSet):
     """Park Areas for Kennywood Amusement Park"""
 
+    # Method that handles creating a single park
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized ParkArea instance
+        """
+        newarea = ParkArea()
+        newarea.name = request.data["name"]
+        newarea.theme = request.data["theme"]
+        newarea.save()
+
+        serializer = ParkAreaSerializer(newarea, context={'request': request})
+
+        return Response(serializer.data)
+
     def retrieve(self, request, pk=None):
         """Handle GET requests for single park area
 
@@ -37,14 +53,46 @@ class ParkAreas(ViewSet):
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
-            
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a park area
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        area = ParkArea.objects.get(pk=pk)
+        area.name = request.data["name"]
+        area.theme = request.data["theme"]
+        area.save()
+
+        # The HTTP 204 No Content success status response code indicates that the request has succeeded, but that the client doesn't need to go away from its current page. (MDN Docs)
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single park area
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            area = ParkArea.objects.get(pk=pk)
+            area.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except ParkArea.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def list(self, request):
         """Handle GET requests to park areas resource
 
         Returns:
             Response -- JSON serialized list of park areas
         """
-        # It's an abstraction that the Object Relational Mapper (ORM) in Django provides that queries the table holding all the park areas, and returns every row. It is the equivalent of you explicitly writing the following SQL. (Nashville Software School, Chapter Docs)
+        # The Object Relational Mapper (ORM) that Django provides that queries the table holding all the park areas and returns every row. It is the equivalent of you explicitly writing SQL. (Nashville Software School, Chapter Docs)
         areas = ParkArea.objects.all()
         serializer = ParkAreaSerializer(
             areas,
