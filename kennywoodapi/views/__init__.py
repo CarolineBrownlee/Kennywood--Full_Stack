41b71553710508eb23673825874622aeecf6f7e1
Caroline Brownlee <brownleecaroline@gmail.com>
2020-02-19 10:34:22 -0600

    added related_name to area property in attraction.py, added import for attractions and itinerary items in __init__.py in views folder, added file itineraryItem.py in views and added code, added file attraction.py in views and added code

--- kennywoodapi/views/__init__.py
+++ kennywoodapi/views/__init__.py
@@ -1,3 +1,5 @@
 from .parkarea import ParkAreas
+from .attraction import Attractions
+from .itineraryItem import ItineraryItems
 from .register import register_user
 from .register import login_user
